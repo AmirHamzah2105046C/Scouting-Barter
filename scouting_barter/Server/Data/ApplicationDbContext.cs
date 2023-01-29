e11783a09cab9d87d482886fbc9eb2adf9ba7082
BuildTools <unconfigured@null.spigotmc.org>
2023-01-29 16:44:40 +0800

    Add project files.

---
+++ scouting_barter/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using scouting_barter.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scouting_barter.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
